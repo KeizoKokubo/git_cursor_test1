2e92b7d4d3885c8eb18e50003fa0f2b6d453e030
Kokubo Keizo <Kokubo.Keizo@bit-science.com>
2026-03-27 19:23:12 +0900

    feat: プロジェクトの初期設定ファイルを追加

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string heroName;
+    int slimeHP = 10;
+
+    cout << "=== Git Quest ===" << endl;
+    cout << "勇者よ、名前を入力せよ: ";
+    cin >> heroName;
+
+    cout << "\n" << heroName << " が現れた！" << endl;
+    cout << "野生のスライムが現れた！ (HP: " << slimeHP << ")" << endl;
+
+    cout << "\n" << heroName << " の攻撃！" << endl;
+    cout << "スライムに 10 のダメージ！" << endl;
+    
+    slimeHP -= 10;
+
+    if (slimeHP <= 0) {
+        cout << "スライムを倒した！" << endl;
+        cout << heroName << " は 経験値を 5 獲得した！" << endl;
+    }
+
+    return 0;
+}
